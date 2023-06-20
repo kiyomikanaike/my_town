ed1713203c2ad5d1da0bae50f39c17323609a70c
kiyomikanaike <kiyomi.kanaike@gmail.com>
2023-06-20 14:35:50 +0000

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :posts
+  devise_for :members
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #会員側のルーティング設定
+  root to:'public/homes#top'#会員側トップページ
+
+  #管理者側のルーティング設定
+
 end
