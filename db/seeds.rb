2a5f9d38f771ae144afbb7f0de8482317c2927ee
kiyomikanaike <kiyomi.kanaike@gmail.com>
2023-08-19 12:21:46 +0000

    [UPDATE]seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,59 +20,48 @@ tohoku.prefectures.find_or_create_by!(name: "宮城県")
 tohoku.prefectures.find_or_create_by!(name: "秋田県")
 tohoku.prefectures.find_or_create_by!(name: "山形県")
 tohoku.prefectures.find_or_create_by!(name: "福島県")
-
+kanto.prefectures.find_or_create_by!(name: "茨城県")
+kanto.prefectures.find_or_create_by!(name: "栃木県")
+kanto.prefectures.find_or_create_by!(name: "群馬県")
+kanto.prefectures.find_or_create_by!(name: "埼玉県")
+kanto.prefectures.find_or_create_by!(name: "千葉県")
+kanto.prefectures.find_or_create_by!(name: "東京都")
+kanto.prefectures.find_or_create_by!(name: "神奈川県")
+chubu.prefectures.find_or_create_by!(name: "新潟県")
+chubu.prefectures.find_or_create_by!(name: "富山県")
+chubu.prefectures.find_or_create_by!(name: "石川県")
+chubu.prefectures.find_or_create_by!(name: "福井県")
+chubu.prefectures.find_or_create_by!(name: "山梨県")
+chubu.prefectures.find_or_create_by!(name: "長野県")
+chubu.prefectures.find_or_create_by!(name: "岐阜県")
+chubu.prefectures.find_or_create_by!(name: "静岡県")
+chubu.prefectures.find_or_create_by!(name: "愛知県")
+kinki.prefectures.find_or_create_by!(name: "三重県")
+kinki.prefectures.find_or_create_by!(name: "滋賀県")
+kinki.prefectures.find_or_create_by!(name: "京都府")
+kinki.prefectures.find_or_create_by!(name: "大阪府")
+kinki.prefectures.find_or_create_by!(name: "兵庫県")
+kinki.prefectures.find_or_create_by!(name: "奈良県")
+kinki.prefectures.find_or_create_by!(name: "和歌山県")
+chugoku.prefectures.find_or_create_by!(name: "鳥取県")
 shimane = chugoku.prefectures.find_or_create_by!(name: "島根県")
 okayama = chugoku.prefectures.find_or_create_by!(name: "岡山県")
+chugoku.prefectures.find_or_create_by!(name: "広島県")
+chugoku.prefectures.find_or_create_by!(name: "山口県")
+shikoku.prefectures.find_or_create_by!(name: "徳島県")
+shikoku.prefectures.find_or_create_by!(name: "香川県")
+shikoku.prefectures.find_or_create_by!(name: "愛媛県")
+shikoku.prefectures.find_or_create_by!(name: "高知県")
+kyushu.prefectures.find_or_create_by!(name: "福岡県")
+kyushu.prefectures.find_or_create_by!(name: "佐賀県")
+kyushu.prefectures.find_or_create_by!(name: "長崎県")
+kyushu.prefectures.find_or_create_by!(name: "熊本県")
+kyushu.prefectures.find_or_create_by!(name: "大分県")
+kyushu.prefectures.find_or_create_by!(name: "宮崎県")
+kyushu.prefectures.find_or_create_by!(name: "鹿児島県")
+kyushu.prefectures.find_or_create_by!(name: "沖縄県")
 
-# prefectures = Prefecture.create!([
-#   { region_id: 1, name: "北海道" },
-#   { region_id: 2, name: "青森県" },
-#   { region_id: 2, name: "岩手県" },
-#   { region_id: 2, name: "宮城県" },
-#   { region_id: 2, name: "秋田県" },
-#   { region_id: 2, name: "山形県" },
-#   { region_id: 2, name: "福島県" },
-#   { region_id: 3, name: "茨城県" },
-#   { region_id: 3, name: "栃木県" },
-#   { region_id: 3, name: "群馬県" },
-#   { region_id: 3, name: "埼玉県" },
-#   { region_id: 3, name: "千葉県" },
-#   { region_id: 3, name: "東京都" },
-#   { region_id: 3, name: "神奈川県" },
-#   { region_id: 4, name: "新潟県" },
-#   { region_id: 4, name: "富山県" },
-#   { region_id: 4, name: "石川県" },
-#   { region_id: 4, name: "福井県" },
-#   { region_id: 4, name: "山梨県" },
-#   { region_id: 4, name: "長野県" },
-#   { region_id: 4, name: "岐阜県" },
-#   { region_id: 4, name: "静岡県" },
-#   { region_id: 4, name: "愛知県" },
-#   { region_id: 5, name: "三重県" },
-#   { region_id: 5, name: "滋賀県" },
-#   { region_id: 5, name: "京都府" },
-#   { region_id: 5, name: "大阪府" },
-#   { region_id: 5, name: "兵庫県" },
-#   { region_id: 5, name: "奈良県" },
-#   { region_id: 5, name: "和歌山県" },
-#   { region_id: 6, name: "鳥取県" },
-#   { region_id: 6, name: "島根県" },
-#   { region_id: 6, name: "岡山県" },
-#   { region_id: 6, name: "広島県" },
-#   { region_id: 6, name: "山口県" },
-#   { region_id: 7, name: "徳島県" },
-#   { region_id: 7, name: "香川県" },
-#   { region_id: 7, name: "愛媛県" },
-#   { region_id: 7, name: "高知県" },
-#   { region_id: 8, name: "福岡県" },
-#   { region_id: 8, name: "佐賀県" },
-#   { region_id: 8, name: "長崎県" },
-#   { region_id: 8, name: "熊本県" },
-#   { region_id: 8, name: "大分県" },
-#   { region_id: 8, name: "宮崎県" },
-#   { region_id: 8, name: "鹿児島県" },
-#   { region_id: 8, name: "沖縄県" }
-# ])
+
 
 member1 = Member.find_or_create_by!(email: "shimane@example.com") do |member|
   member.birth_date = Time.parse("1960/01/01")
